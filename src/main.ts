aa801c14e570ba6834626692f26c9bb874ee7e4d
IamNeoleon <kusherbaev.bakhtiyar@gmail.com>
2026-03-06 16:30:04 +0500

    chore: add frontend cors url

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
 		origin: [
 			'http://localhost:5173',
 			'http://localhost:4173',
+			'https://flow-up-web.vercel.app'
 		],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
